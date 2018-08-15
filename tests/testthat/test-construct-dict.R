bb47ae0cb1b1e61f00cd9ca78375ba0ea19ee79a
liao@nb <liaomovie2@gmail.com>
2018-08-15 13:21:38 +0800

    Set test for 'ptt_dict' format

--- tests/testthat/test-construct-dict.R
+++ tests/testthat/test-construct-dict.R
@@ -1,3 +1,7 @@
-context("dictionary construction")
+context("data-raw/scrap-ptt-wiki.R")
 
-test_that()
+test_that("ptt_dict is in right format", {
+  expect_equal(colnames(ptt_dict), c("term", "source"))
+  expect_true(nrow(ptt_dict) >= 100)
+  expect_equal(class(attr(ptt_dict, "date")), "Date")
+})
