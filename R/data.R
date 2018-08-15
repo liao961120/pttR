c39409b192f80663a2e9773822cc7fb29fe8cd63
liao@nb <liaomovie2@gmail.com>
2018-08-15 12:05:48 +0800

    Document external dataset: 'ptt_dict'

---
+++ R/data.R
@@ -0,0 +1,28 @@
+#' A dictionary for Ptt
+#'
+#' A dataset containing Ptt-specific terms scraped from
+#' \href{http://zh.pttpedia.wikia.com}{pttpedia}.
+#'
+#' @format A data frame with 2 variables
+#' \describe{
+#'   \item{term}{Ptt-specific terms}
+#'   \item{source}{wiki pages where the terms are scraped
+#'    \describe{
+#'      \item{\href{https://bit.ly/2nEfrQF}{board_name}}
+#'      \item{\href{https://bit.ly/2OBli4y}{event_name}}
+#'      \item{\href{https://bit.ly/2MeDG6E}{famous_people}}
+#'      \item{\href{https://bit.ly/25ub22j}{culture}}
+#'      \item{\href{https://bit.ly/2KTAzvx}{notations}}
+#'      \item{\href{https://bit.ly/2KPHTZ6}{basic_terms}}
+#'      \item{\href{https://bit.ly/2BdKQmR}{post_type}}
+#'      \item{\href{https://bit.ly/2w6oQnX}{newest}}
+#'    }
+#'   }
+#' }
+#' @source \url{http://zh.pttpedia.wikia.com}
+#' @examples
+#' head(ptt_dict)
+#'
+#' # get the date of the newest page
+#' attr(ptt_dict, "newest_page")
+"ptt_dict"
