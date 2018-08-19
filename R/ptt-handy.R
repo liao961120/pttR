da3c2fdbba81f92609efe74e7820e29bb4bb6a5a
liao@nb <liaomovie2@gmail.com>
2018-08-19 12:56:38 +0800

    Move and rename function

--- R/ptt-handy.R
+++ R/ptt-handy.R
@@ -21,3 +21,72 @@ ptt <- function() {
   colnames(df) <- NULL
   df
 }
+
+
+#' Return a data frame with popular boards info
+#'
+#' \code{hotboards} returns a data frame of
+#' popular boards (\emph{\enc{熱門看板}{re men kan ban}})
+#' on PTT.
+#'
+#' @param get_new Logical. Defaults to \code{FALSE}.
+#'   If \code{TRUE}, scrapes and retreive data from
+#'   \url{https://www.ptt.cc/bbs/hotboards.html}.
+#'   If \code{FALSE}, use pre-scraped data stored in
+#'   the package.
+#'
+#' @examples
+#' df <- hotboards()
+#' head(df)
+#'
+#' # Get data update time
+#' attr(df, "date")
+#'
+#' @source \url{https://www.ptt.cc/bbs/hotboards.html}
+#'
+#' @importFrom rvest html_nodes html_attr html_text
+#' @importFrom xml2 read_html
+#' @importFrom stringr str_replace str_extract str_remove
+#' @importFrom dplyr %>% bind_cols mutate
+#' @export
+hotboards <- function(get_new = FALSE) {
+
+  if (!get_new) return(hotboard_df)
+
+  link <- "https://www.ptt.cc/bbs/hotboards.html"
+
+  hotboards <- read_html(link) %>%
+    html_nodes("div.b-ent") %>%
+    html_nodes("a.board")
+
+  board_link <- hotboards %>%
+    html_attr("href") %>%
+    str_replace("^/", "https://www.ptt.cc/")
+
+  board_name_en <- hotboards %>%
+    html_nodes("div.board-name") %>%
+    html_text()
+
+  board_name_ch <- hotboards %>%
+    html_nodes("div.board-title") %>%
+    html_text() %>%
+    str_extract("^\u25ce\\[.+\\]") %>% # \u25ce : ◎
+    str_remove("\u25ce\\[") %>%
+    str_remove("\\]$")
+
+  board_popularity <- hotboards %>%
+    html_nodes("div.board-nuser > span") %>%
+    html_text()
+
+  df <- cbind(board = board_name_en,
+              name_ch = board_name_ch,
+              popularity = board_popularity,
+              link = board_link)
+  df$name_ch <- ifelse(is.na(df$name_ch),
+                                df$board,
+                                df$name_ch)
+
+  attr(df, "date") <- Sys.time()
+
+  return(df)
+}
