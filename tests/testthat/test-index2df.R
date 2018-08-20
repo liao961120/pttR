d720ee8eec0ef14e2d81f7ff264e7acbba92433c
liao@nb <liaomovie2@gmail.com>
2018-08-20 16:45:00 +0800

    Rewrite test for index2df due to rewriting the funcion

--- tests/testthat/test-index2df.R
+++ tests/testthat/test-index2df.R
@@ -1,7 +1,7 @@
 context("index2df() and helpers")
 
-# index2df(board, n = 1, range = c(NA, NA),
-#          search = c(term = NA, from = 1, to = 2))
+# index2df(board, newest = vec(1), pages = vec(NA),
+#          search_term = chr(NA), search_page = vec(1))
 
 ## Global variables --------
 n_var <- 7  # Number of columns returned
@@ -10,61 +10,55 @@ n_var <- 7  # Number of columns returned
 
 test_that("arg.board input", {
   expect_error(index2df("gossip"))
-  expect_error(index2df("http", search = c("a", 1, 1)),
-               "[Ii]nvalid board name")
+  expect_error(index2df("gossiping", newest = 0),
+               "'newest' must be a positive int")
 })
 
 
-## Test Ordered: Argument 'n' ------------
+## Test: Argument 'newest' ------------
 df <- index2df("gossiping")
-df1 <- index2df("gossiping", n = 2)
+df1 <- index2df("gossiping", newest = 2)
 uniq_val <- min(unique(df1$idx_n))
 uniq_len <- length(unique(df1$idx_n))
-df2 <- index2df("gossiping", range = c(1, 1))
 url <- "https://www.ptt.cc/bbs/NCCU_EDUGRAD/index.html" #cold board
 
-test_that("check_index2df_order() arg.n", {
-  expect_error(index2df(url, n = 500000))
-  expect_error(index2df("gossiping", n = 0), "Invalid n")
-  expect_true(nrow(df) <= 25)
-  expect_true(nrow(df1) >= 30)
+test_that("index2df arg.newest", {
+  expect_error(index2df(url, newest = 50000000),
+               "Range exceeds possible")
+  expect_true(nrow(df) <= 30)
+  expect_true(nrow(df1) >= nrow(df))
   expect_true(ncol(df1) >= n_var)
   expect_true(uniq_val >= 38815)
   expect_equal(uniq_len, 2)  # n = 2
 })
 
 
-## Test Ordered: Argument 'range' ------------
-df <- index2df("gossiping", range = c(1554, 1555))
-uniq_val <- min(unique(df$idx_n))
-uniq_len <- length(unique(df$idx_n))
+## Test Ordered: Argument 'pages' ------------
+df2 <- index2df("gossiping", pages = c(1, 3, 5))
+uniq_val <- min(unique(df2$idx_n))
+uniq_len <- length(unique(df2$idx_n))
 
-test_that("check_index2df_order() arg.range", {
-  expect_error(index2df("gossiping", range = c(NA, 1)),
-               "Arg. 'range' needs two integer")
-  expect_error(index2df("gossiping", range = c(1, NA)))
-  expect_equal(uniq_val, 1554)
-  expect_equal(uniq_len, 2)
-  expect_true(nrow(df) >= 30)
-  expect_true(ncol(df) >= n_var)
+test_that("index2df arg.pages", {
+  expect_equal(uniq_val, 1)
+  expect_equal(uniq_len, 3)
+  expect_true(nrow(df2) >= 30)
+  expect_true(ncol(df2) >= n_var)
 })
 
-## Test Searching: Argument 'search' -------------
+## Test Searching: Argument 'search_term' -------------
 
 # cold board: NCCU_EDUGRAD
 board <- "NCCU_EDUGRAD"
-df <- index2df(board, search = c("教", 1, 2))
+df <- index2df(board, search_term = "教", search_page = 1:2)
 uniq_val <- unique(df$idx_n)[1]
 uniq_len <- length(unique(df$idx_n))
 
-test_that("check_index2df_order() arg.range", {
-  expect_error(index2df("gossiping", range = c(NA, 1)),
-               "Arg. 'range' needs two integer")
-  expect_error(index2df("gossiping", range = c(1, NA)))
+test_that("index2df arg.search_term", {
   expect_equal(uniq_val, 1)
   expect_equal(uniq_len, 2)
   expect_true(nrow(df) >= 30)
   expect_true(ncol(df) >= n_var)
-  expect_error(index2df(board, search = c("教", 1, 5)),
+  expect_error(index2df(board, search_term = "教",
+                        search_page = 1:5),
                "Page Not Found")
 })
