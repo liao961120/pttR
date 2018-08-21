86a4d44ebdcfae0cfe6167d57c5a57566a2d7305
liao@nb <liaomovie2@gmail.com>
2018-08-21 16:40:36 +0800

    A fun to open web page in browser

---
+++ R/interact-fun.R
@@ -0,0 +1,15 @@
+#' Open web page in default browser
+#' @keywords internal
+browse <- function(URL = "https://liao961120.github.io/pttR/") {
+  cat("Visiting ", URL, "...\n")
+  Sys.sleep(1)
+  cat("Press 'Esc' to stop.\n")
+  Sys.sleep(1)
+  cat("Opening Browser in 3\n")
+  Sys.sleep(1)
+  for (i in 2:1) {
+    cat("                  ", i,"\n")
+    Sys.sleep(1)
+  }
+  browseURL(URL)
+}
