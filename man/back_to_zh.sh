0f295cda5fea75a73cbc2a2ee9018d10b48f1e10
liao@nb <liaomovie2@gmail.com>
2018-08-22 16:15:07 +0800

    Fix script

--- man/back_to_zh.sh
+++ man/back_to_zh.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-if [[ ! -d translated ]]; then mkdir translated; fi
+mkdir pingyin_dir
+cp -r *.Rd pingyin_dir  # Backup file for win-build
+
 
 trans="../vignettes/translation.csv"
 n_line=$(wc -l $trans | grep -o "[0-9]")
 
-for (( i=1; i<=${n_line}; ++i )); do
-	line=$(sed -n -e "$i"p $trans)
-	zh=$(echo $line | cut -f 1 -d ,)
-	pingyin=$(echo $line | cut -f 2 -d ,)
-	
-	for f in *.Rd; do
-		cat "${f}" | sed -r "s/$pingyin/$zh/g" > translated/"${f}"
+for f in *.Rd; do
+	temp=
+	for (( i=1; i<=${n_line}; ++i )); do
+		line=$(sed -n -e "$i"p $trans)
+		zh=$(echo $line | cut -f 1 -d ,)
+		pingyin=$(echo $line | cut -f 2 -d ,)
+
+		cat "${f}" | sed -r "s/$pingyin/$zh/g" > "temp"
+		cat "temp" > ${f}
 	done
 done
 
-cp -r translated/*.Rd .
-rm -r translated
+rm temp
