cc2c3ebac7535b89749da71c11949242420a175d
liao@nb <liaomovie2@gmail.com>
2018-08-23 19:32:26 +0800

    Fix test

--- tests/testthat/test-get_post-comment.R
+++ tests/testthat/test-get_post-comment.R
@@ -5,15 +5,12 @@ eval_str <- function(str) eval(parse(text = str))
 
 url <- "https://www.ptt.cc/bbs/Gossiping/M.1514735254.A.8ED.html"
 df0 <- get_post(url)
-df1 <- get_post(url, , index = 1L)
-df2 <- get_post(url, index = 1L, board_col = T)
+df1 <- get_post(url, board_col = T)
 
 test_that("dimension of get_post()",{
   expect_equal(ncol(df0) + 1, ncol(df1))
-  expect_equal(ncol(df1) + 1, ncol(df2))
   expect_equal(nrow(df1), 1)
   expect_true(nrow(df0) == nrow(df1))
-  expect_true(nrow(df2) == nrow(df1))
 })
 
 
