95151382e0a62d8713b0ac2775a7244a4846493b
liao@nb <liaomovie2@gmail.com>
2018-08-23 19:34:14 +0800

    New data

--- data-raw/write-data2internal.R
+++ data-raw/write-data2internal.R
@@ -1,6 +1,8 @@
 ptt_dict <- readRDS("data-raw/ptt_dict.rds")
 hotboard_df <- readRDS("data-raw/hotboard_df.rds")
 pingying2zh <- read.csv("data-raw/translation.csv", header = F)
+posts_df <- readRDS("data-raw/posts_df.rds")
 
 devtools::use_data(ptt_dict, hotboard_df, pingying2zh,
+                   posts_df,
                    internal = T, overwrite = T)
