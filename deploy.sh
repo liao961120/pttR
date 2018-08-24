7002aaa10935b572bff831111e4ef42ba32edeae
liao@nb <liaomovie2@gmail.com>
2018-08-24 22:02:13 +0800

    cp favicon/ to docs/

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
   Rscript -e 'source("build_site.R")'  # Build pkgdown site with man/ in zh
   ### Add favicon links to every html head
   head='<link rel="apple-touch-icon" sizes="180x180" href="https://liao961120.github.io/pttR/favicon/apple-touch-icon.png"><link rel="icon" type="image/png" sizes="32x32" href="https://liao961120.github.io/pttR/favicon/favicon-32x32.png"><link rel="icon" type="image/png" sizes="16x16" href="https://liao961120.github.io/pttR/favicon/favicon-16x16.png"><link rel="manifest" href="https://liao961120.github.io/pttR/favicon/site.webmanifest"><link rel="mask-icon" href="https://liao961120.github.io/pttR/favicon/safari-pinned-tab.svg" color="#d57700"><link rel="shortcut icon" href="https://liao961120.github.io/pttR/favicon/favicon.ico"><meta name="apple-mobile-web-app-title" content="pttR"><meta name="application-name" content="pttR"><meta name="msapplication-TileColor" content="#00a300"><meta name="msapplication-TileImage" content="https://liao961120.github.io/pttR/favicon/mstile-144x144.png"><meta name="msapplication-config" content="https://liao961120.github.io/pttR/favicon/browserconfig.xml"><meta name="theme-color" content="#ffffff">'
-  
+
   cd ./docs
     file=$(find . -name '*.html')  
     for i in $file; do
