152a9a2e628fc0bb3a04eaceb688b7f86f8aff18
liao@nb <liaomovie2@gmail.com>
2018-08-25 08:48:32 +0800

    update favicon

--- deploy.sh
+++ deploy.sh
@@ -21,9 +21,9 @@ if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
   ## Modify head region of built HTML for optimizing SEO
      # head: Replace link for supportting multi-platform favicon
      # head1, head2: Change image path to direcet URL (May be a bug in pkgdown), and rescale image
-  head='<link rel="apple-touch-icon" sizes="180x180" href="https://liao961120.github.io/pttR/favicon/apple-touch-icon.png"><link rel="icon" type="image/png" sizes="32x32" href="https://liao961120.github.io/pttR/favicon/favicon-32x32.png"><link rel="icon" type="image/png" sizes="16x16" href="https://liao961120.github.io/pttR/favicon/favicon-16x16.png"><link rel="manifest" href="https://liao961120.github.io/pttR/favicon/site.webmanifest"><link rel="mask-icon" href="https://liao961120.github.io/pttR/favicon/safari-pinned-tab.svg" color="#d57700"><link rel="shortcut icon" href="https://liao961120.github.io/pttR/favicon/favicon.ico"><meta name="apple-mobile-web-app-title" content="pttR"><meta name="application-name" content="pttR"><meta name="msapplication-TileColor" content="#00a300"><meta name="msapplication-TileImage" content="https://liao961120.github.io/pttR/favicon/mstile-144x144.png"><meta name="msapplication-config" content="https://liao961120.github.io/pttR/favicon/browserconfig.xml"><meta name="theme-color" content="#ffffff">'
-  head1='<meta property="og:image" content="/logo.png">'
-  head2='<meta property="og:image" content="https://liao961120.github.io/pttR/logo.png"><meta property="og:image:width" content="205"><meta property="og:image:height" content="205">'
+  head='<link rel="apple-touch-icon" sizes="180x180" href="https://liao961120.github.io/pttR/favicon/apple-touch-icon.png?v=alQmNKw6dL"><link rel="icon" type="image/png" sizes="32x32" href="https://liao961120.github.io/pttR/favicon/favicon-32x32.png?v=alQmNKw6dL"><link rel="icon" type="image/png" sizes="194x194" href="https://liao961120.github.io/pttR/favicon/favicon-194x194.png?v=alQmNKw6dL"><link rel="icon" type="image/png" sizes="192x192" href="https://liao961120.github.io/pttR/favicon/android-chrome-192x192.png?v=alQmNKw6dL"><link rel="icon" type="image/png" sizes="16x16" href="https://liao961120.github.io/pttR/favicon/favicon-16x16.png?v=alQmNKw6dL"><link rel="manifest" href="https://liao961120.github.io/pttR/favicon/site.webmanifest?v=alQmNKw6dL"><link rel="mask-icon" href="https://liao961120.github.io/pttR/favicon/safari-pinned-tab.svg?v=alQmNKw6dL" color="#d56000"><link rel="shortcut icon" href="https://liao961120.github.io/pttR/favicon/favicon.ico?v=alQmNKw6dL"><meta name="apple-mobile-web-app-title" content="pttR"><meta name="application-name" content="pttR"><meta name="msapplication-TileColor" content="#00a300"><meta name="msapplication-TileImage" content="https://liao961120.github.io/pttR/favicon/mstile-144x144.png?v=alQmNKw6dL"><meta name="msapplication-config" content="https://liao961120.github.io/pttR/favicon/browserconfig.xml?v=alQmNKw6dL"><meta name="theme-color" content="#ffffff">'
+  head_og1='<meta property="og:image" content="/logo.png">'
+  head_og2='<meta property="og:image" content="https://liao961120.github.io/pttR/favicon/og-image.jpg"><meta property="og:image:width" content="170"><meta property="og:image:height" content="170">'
 
   cd ./docs
     file=$(find . -name '*.html')  
@@ -31,12 +31,13 @@ if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
       cat "${i}" | sed "s,<head>,<head>${head},g" > "temp"
       cat "temp" > "${i}"
       
-      cat "${i}" | sed "s,${head1},${head2},g" > "temp"
+      cat "${i}" | sed "s,${head_og1},${head_og2},g" > "temp"
       cat "temp" > "${i}"
     done
     rm temp
   cd ..
   mv ./favicon/ ./docs/
+  rm ./docs/favicon.ico
 
 
   # PART 3: Prepare for deploy to win-build
