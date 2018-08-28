cd3ef0925c6e7590e5883bacbda3a71bda7a52cc
liao@nb <liaomovie2@gmail.com>
2018-08-28 17:33:50 +0800

    compress data

---
+++ tests/testthat/test-input-post-format.R
@@ -0,0 +1,10 @@
+context("Test various possible input post formats")
+
+## Posts with Author replies in comments
+# "https://www.ptt.cc/bbs/Baseball/M.1535372407.A.902.html")
+# "https://www.ptt.cc/bbs/car/M.1500309737.A.D47.html")
+
+## Posts with No comments
+# system.file("ntucourse/posts", "M.1534838066.A.27C.html.gz", package = "pttR")
+
+## Posts with cross-year comments
