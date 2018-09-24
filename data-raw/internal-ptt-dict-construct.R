855c20373f8af304056ff3fc167031c6ea9d249f
liao <liaomovie2@gmail.com>
2018-09-24 21:46:10 +0800

    new data

--- data-raw/internal-ptt-dict-construct.R
+++ data-raw/internal-ptt-dict-construct.R
@@ -4,14 +4,6 @@ library(stringr)
 raw <- readr::read_csv(url("https://liao961120.github.io/PTT-scrapy/dict.csv"))
 
 ptt_dict <- raw %>%
-  filter(nchar(term) <= 8) %>%
-  filter(!str_detect(term, "\\w+(、|；|x+|X+| )\\w+")) %>%
-  filter(!str_detect(term, "^\\[.+\\]")) %>%
-  filter(!str_detect(term, "^[a-zA-Z0-9]+$")) %>%
-  mutate(term = str_remove_all(term, "^[、|「]")) %>%
-  mutate(term = str_remove_all(term, "[」!?？！、，]$")) %>%
-  mutate(term = str_remove(term, "^(\\.+|…+)")) %>%
-  mutate(term = str_remove(term, "(\\.+|…+)$")) %>%
   filter(nchar(term) > 1) %>%
   distinct(term, .keep_all = T) %>%
   na.omit()
