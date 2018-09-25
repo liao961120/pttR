0d713218168e37648da451794610d2954e4c6bbc
liao <liaomovie2@gmail.com>
2018-09-25 17:28:11 +0800

    New function: segmentation

--- R/seg.R
+++ R/seg.R
@@ -1,35 +1,32 @@
+#' @rdname segment
+#' @export
+seg_content <- function(x, words = NULL, tags = NULL, user = NULL) {
+  # alias of seg_ptt
+  seg_ptt(x, words = words, tags = tags, user = user)
+}
 
-## idx_df <- index2df("gossiping",
-##                    search_term = '林飛帆',
-##                    search_page = 1:5)
+#' @rdname segment
+#' @export
+seg_comment <- function(x, words = NULL, tags = NULL, user = NULL) {
+  # x is a list column with df in every element
+  for (i in seq_along(x)) {
+    if (is.null(x[[i]])) break
+    df <- x[[i]]
+    df$comment <- seg_ptt(df$comment, words = words,
+                          tags = tags, user = user)
+    x[[i]] <- df
+  }
+  return(x)
+}
 
-## pttR::down_html(as_url(idx_df$link), dir = "local/fan/")
-
-#library(pttR)
-#post_df <- post2df(list.files('local/fan', full.names = T))
 
 
 #' Word segmentation for PTT post content
 #'
-#' @param x Character vector or data frame. Text to be
-#'   segmented. If a data frame is passed, the column
-#'   'content' or 'comment' will be extracted and
-#'   use as the input character vector.
-#' @param words Character vector. A vector of words to
-#'   pass to jiebaR dictionary.
-#'   See \code{\link[jiebaR]{new_user_word}} for details.
-#' @param tags Character vector. A vector of tags
-#'   specifying the lexical categories of the words in
-#'   `words`. Defaults to `n` (noun).
-#'   See \code{\link[jiebaR]{new_user_word}} for details.
-#' @param user Character. A string specifying the path to
-#'   user defined dictionary. Defaults to pttR built-in
-#'   dictionary. See
-#'   \href{jiebaR doc}{https://qinwenfeng.com/jiebaR/worker-.html#user-} for details.
-#' @source \url{https://liao961120.github.io/PTT-scrapy/}
+#' @param x Character vector. Text to be segmented.
 #' @importFrom jiebaR worker new_user_word segment
+#' @keywords internal
 seg_ptt <- function(x, words = NULL, tags = NULL, user = NULL) {
-  stopifnot((is.character(x) || is.data.frame(x)))
 
   # Set up jiebaR seg function
   if (is.null(user)) user <- system.file('pttdict/pttdict.csv', package = 'pttR')
@@ -39,21 +36,33 @@ seg_ptt <- function(x, words = NULL, tags = NULL, user = NULL) {
     new_user_word(seg, words = words, tags = tags)
   }
   # Main function
-  if (is.data.frame(x)) {
-    if ('content' %in% colnames(x)) {
-      x <- x$content
-    } else if ('comment' %in% colnames(x)) {
-      x <- x$comment
-    } else {
-      stop('Invalid input data frame.
-           Must contain column "content" or "comment".')
-    }
-  }
   segged <- lapply(segment(x, seg),
-                   function(x) paste(x, collapse = ' ')
-                  )
+                   function(x) paste(x, collapse = ' '))
   return(unlist(segged))
 }
 
 
 
+#### Common Documentation ####
+
+#' Word segmentation for PTT post content and comments.
+#'
+#' @param x Column 'content' or 'comment' from a data frame
+#'   returned by \code{post2df}.
+#' @param words Character vector. A vector of words to
+#'   pass to jiebaR dictionary.
+#'   See \code{\link[jiebaR]{new_user_word}} for details.
+#' @param tags Character vector. A vector of tags
+#'   specifying the lexical categories of the words in
+#'   `words`. Defaults to `n` (noun).
+#'   See \code{\link[jiebaR]{new_user_word}} for details.
+#' @param user Character. A string specifying the path to
+#'   an user defined dictionary. Defaults to pttR built-in
+#'   dictionary. See
+#'   \href{jiebaR doc}{https://qinwenfeng.com/jiebaR/worker-.html#user-}
+#'   for details.
+#' @source
+#'   For details about the built-in ptt dictionary,
+#'   see \url{https://liao961120.github.io/PTT-scrapy/}.
+#' @name segment
+NULL
