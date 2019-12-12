acbbc5a7d80222a4db836eb65de7527ca2c6b774
liao <liao961120@gmail.com>
2019-12-12 16:29:30 +0800

    try fix encoding error

--- R/data-retrieve.R
+++ R/data-retrieve.R
@@ -18,13 +18,13 @@
 #' @importFrom xml2 read_html
 #' @export
 read_html2 <- function(url, ...) {
-  if (!grepl("^http", url)) return(read_html(url, ...))
+  if (!grepl("^http", url)) return(read_html(url, encoding = "utf-8"...))
   curl_1 <- getCurlHandle()
   curlSetOpt(cookie = "over18=1",
              followlocation = TRUE,
              curl = curl_1)
  url <- getURL(url, curl = curl_1)
- read_html(url, ...)
+ read_html(url, encoding = "utf-8", ...)
 }
 
 #' Dowload HTML files to local directory
